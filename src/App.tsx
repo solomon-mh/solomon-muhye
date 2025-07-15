010466700382d43a485dfa7f4713e838efcf6373
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-16 00:59:04 +0300

    refactor App component layout and structure for improved organization and visual effects

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,35 @@
-import Hero from "./components/Hero";
 import About from "./components/About";
 import SkillsAndTools from "./components/SkillsAndTools";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Experience from "./components/Experience";
+import FloatingMenu from "./components/FloatingMenu";
+import FloatingToggle from "./components/FloatingToggler";
+import CursorRipple from "./components/CursorRipple";
+import HeroSection from "./components/Hero";
 
 export default function App() {
   return (
-    <div className="font-sans text-gray-800 dark:text-white bg-white dark:bg-gray-900">
-      {/* 1. Hero Section */}
-      <Hero />
+    <div className="font-sans text-gray-800 dark:text-white dark:bg-gray-900 transition-colors duration-1000">
+      <CursorRipple />
+      <FloatingToggle />
+      <main className="lg:flex gap-12 px-12 lg:px-24">
+        {/* Left: Fixed Hero Section */}
+        <div className="block lg:w-1/2 h-screen lg:sticky top-0">
+          <HeroSection />
+        </div>
 
-      {/* 2. About Me */}
-      <About />
-
-      {/* 3. Skills & Tools */}
-      <SkillsAndTools />
-
-      {/* 4. Projects */}
-      <Projects />
-
-      {/* 5. Experience */}
-      <Experience />
-
-      {/* 6. Contact */}
-      <Contact />
-
+        {/* Right: Scrollable Content */}
+        <div className="lg:w-1/2 space-y-20 px-4 lg:px-12 py-10 overflow-y-auto">
+          <About />
+          <SkillsAndTools />
+          <Projects />
+          <Experience />
+          <Contact />
+        </div>
+      </main>
+      <FloatingMenu />
       {/* 7. Footer */}
       <Footer />
     </div>
