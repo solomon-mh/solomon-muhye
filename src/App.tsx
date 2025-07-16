14b6064cc3a81f68bf15bb4362f3fda1ad9829c1
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-17 01:19:19 +0300

    add ToastContainer for enhanced notification handling in App component

--- src/App.tsx
+++ src/App.tsx
@@ -8,30 +8,42 @@ import FloatingMenu from "./components/FloatingMenu";
 import FloatingToggle from "./components/FloatingToggler";
 import CursorRipple from "./components/CursorRipple";
 import HeroSection from "./components/Hero";
+import { Slide, ToastContainer } from "react-toastify";
 
 export default function App() {
   return (
-    <div className="font-sans text-gray-800 dark:text-white dark:bg-gray-900 transition-colors duration-1000">
-      <CursorRipple />
-      <FloatingToggle />
-      <main className="lg:flex gap-12 px-12 lg:px-24">
-        {/* Left: Fixed Hero Section */}
-        <div className="block lg:w-1/2 h-screen lg:sticky top-0">
-          <HeroSection />
-        </div>
-
-        {/* Right: Scrollable Content */}
-        <div className="lg:w-1/2 space-y-20 px-4 lg:px-12 py-10 overflow-y-auto">
-          <About />
-          <SkillsAndTools />
-          <Projects />
-          <Experience />
-          <Contact />
-        </div>
-      </main>
-      <FloatingMenu />
-      {/* 7. Footer */}
-      <Footer />
-    </div>
+    <>
+      <div className="font-sans text-gray-800 dark:text-white dark:bg-gray-900 transition-colors duration-1000">
+        <CursorRipple />
+        <FloatingToggle />
+        <main className="lg:flex gap-12 px-12 lg:px-24">
+          <div className="block lg:w-1/2 h-screen lg:sticky top-0">
+            <HeroSection />
+          </div>
+          <div className="lg:w-1/2 space-y-20 px-4 lg:px-12 py-10 overflow-y-auto">
+            <About />
+            <SkillsAndTools />
+            <Projects />
+            <Experience />
+            <Contact />
+          </div>
+        </main>
+        <FloatingMenu />
+        <Footer />
+      </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={4200}
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Slide}
+      />
+    </>
   );
 }
