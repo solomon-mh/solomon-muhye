cefbad6ce96fb0e38b6c9d319405d2dec265f6f1
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-26 03:31:03 +0300

    refactor FloatingMenu component to adjust visibility class and simplify icon styling for improved layout consistency

--- src/components/FloatingMenu.tsx
+++ src/components/FloatingMenu.tsx
@@ -31,7 +31,7 @@ const FloatingMenu = () => {
     <AnimatePresence>
       {scrolled && (
         <motion.div
-          className="fixed bottom-24 left-1/2 transform -translate-x-3/5 md:-translate-x-1/2 z-50 flex px-4  md:px-8 py-4 rounded-full shadow-xl backdrop-blur-md bg-white/70 dark:bg-black/40 border border-green-500 dark:border-green-400 space-x-6"
+          className="fixed hidden lg:flex bottom-24 left-1/2 transform -translate-x-3/5 md:-translate-x-1/2 z-50 px-4  md:px-8 py-4 rounded-full shadow-xl backdrop-blur-md bg-white/70 dark:bg-black/40 border border-green-500 dark:border-green-400 space-x-6"
           initial={{ opacity: 0, y: 40 }}
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: 40 }}
@@ -44,7 +44,7 @@ const FloatingMenu = () => {
               whileHover={{ scale: 1.15 }}
               className="flex flex-col items-center text-sm text-green-700 dark:text-green-300 hover:text-green-900 dark:hover:text-green-100 transition-all duration-300"
             >
-              <div className="text-xl">{item.icon}</div>
+              <div>{item.icon}</div>
               <span className="text-xs mt-1 font-medium">{item.label}</span>
             </motion.a>
           ))}
