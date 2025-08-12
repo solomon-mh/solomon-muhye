82f135ddb98200837b9e7369bee2e90b17792f6f
solomon-mh <solomon.muhye.wd@gmail.com>
2025-08-12 11:42:40 +0300

    refactor App component to replace FloatingToggle with HangingToggle for improved functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,17 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Experience from "./components/Experience";
 import FloatingMenu from "./components/FloatingMenu";
-import FloatingToggle from "./components/FloatingToggler";
 import CursorRipple from "./components/CursorRipple";
 import HeroSection from "./components/Hero";
 import { Slide, ToastContainer } from "react-toastify";
+import HangingToggle from "./components/FloatingToggler";
 
 export default function App() {
   return (
     <>
       <div className="text-gray-800 dark:text-white dark:bg-gray-900 transition-colors duration-1000">
         <CursorRipple />
-        <FloatingToggle />
+        <HangingToggle />
         <main className="w-5/6 lg:w-1/2 mx-auto space-y-12">
           <HeroSection />
           <About />
