b91117e0196f2c8efbedb36c48fb65505ee57eb4
solomon-mh <solomon.muhye.wd@gmail.com>
2026-08-12 09:22:58 +0300

    feat: update project titles and remove unused project entries

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,13 +1,9 @@
-import brainwave from "../assets/brainwave.png";
-import jibyte from "../assets/jibyte.png";
 import customEd from "../assets/customEd.png";
 import beteseb from "../assets/beteseb.png";
-import mealer from "../assets/mealer.png";
-import mikander from "../assets/mikander.png";
 
 export const projects = [
   {
-    title: "Smart Pharmacy Finder",
+    title: "Fetanfews | Smart Pharmacy Finder",
     description:
       "I was urgently searching for medications for my family but couldn't find them at nearby pharmacies. So this motivated me to build this website — a location-based pharmacy and medication search engine with real-time availability, search history, and fuzzy logic.",
     tech: ["TailwindCSS", "React", "Laravel", "MySql", "Leaflet"],
@@ -24,7 +20,7 @@ export const projects = [
     image: customEd,
   },
   {
-    title: "beteseb",
+    title: "beteseb | hotel booking and manegement",
     description:
       "A web-based application that simplifies hotel room bookings. Users can browse hotels, book rooms, and manage reservations, while admins manage hotel details, user accounts, and booking requests.",
     tech: [
@@ -40,55 +36,5 @@ export const projects = [
     githubLink:
       "https://github.com/solomon-mh/bahirdar_hotels_room_reservation_frontend",
     image: beteseb,
-  },
-  {
-    title: "mealer",
-    description:
-      "AI based application that helps user to generate a weekly based meal plan based on their preference. It allows user to have different subscription plans and allow them to create a customised user based Weekly meal planner based on the amount of calory intake they want. ",
-    tech: [
-      "Tailwindcss",
-      "NextJs",
-      "Typescript",
-      "FramerMotion",
-      "Clerk",
-      "Stripe",
-    ],
-    demoLink: "https://mealer-snowy.vercel.app/",
-    githubLink: "https://github.com/solomon-mh/mealer",
-    image: mealer,
-  },
-  {
-    title: "mikander",
-    description:
-      "A modern EdTech web application that integrates voice AI capabilities to provide interactive and accessible educational content, enhancing user engagement and learning effectiveness. ",
-    tech: [
-      "Tailwindcss",
-      "NextJs",
-      "Typescript",
-      "FramerMotion",
-      "Supabase",
-      "Clerk",
-    ],
-    demoLink: "https://mikander.vercel.app/",
-    githubLink: "https://github.com/solomon-mh/mikander",
-    image: mikander,
-  },
-  {
-    title: "JibyteEcho",
-    description:
-      "A responsive UI clone of the Jibyte AI website, featuring smooth animations and modern design. JibyteEcho mirrors the original's aesthetics with precision and attention to detail.",
-    tech: ["Tailwindcss", "React", "Javascript"],
-    demoLink: "https://github.com/solomon-mh/ai-website-ui-clone.git",
-    githubLink: "https://github.com/solomon-mh/ai-website-ui-clone.git",
-    image: jibyte,
-  },
-  {
-    title: "Brainwave",
-    description:
-      "A modern AI website landing page built with a sleek, animated UI, with parallax effect and responsive design, showcasing clean visuals and smooth user interactions",
-    tech: ["Tailwindcss", "React", "Framer Motion"],
-    demoLink: "https://github.com/solomon-mh/brainwave",
-    githubLink: "https://github.com/solomon-mh/brainwave",
-    image: brainwave,
   },
 ];
